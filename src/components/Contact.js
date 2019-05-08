4f57ff3db5fbd77732055176aee18b491f283e6d
Sufi <sufi.j30@gmail.com>
2019-05-07 23:20:41 -0400

    implemented navbar
    set up component files

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium deleniti omnis voluptatum reprehenderit et ut, autem nesciunt fugiat distinctio ex dolor odit dolorum corporis. Eos ipsum distinctio delectus quidem eaque!</p>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium deleniti omnis voluptatum reprehenderit et ut, autem nesciunt fugiat distinctio ex dolor odit dolorum corporis. Eos ipsum distinctio delectus quidem eaque!</p>
+        </div>
+    )
+}
+
+export default Home

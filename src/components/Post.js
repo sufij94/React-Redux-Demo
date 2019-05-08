eaa3b0d7fbd38852174911b2a483d7ffbfde3983
Sufi <sufi.j30@gmail.com>
2019-05-08 14:44:46 -0400

    built action creator

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import axios from 'axios'
 import { connect } from 'react-redux'
+import { deletePost } from '../actions/postActions'
 
 class Post extends Component {
     handleClick = () => {
@@ -54,7 +55,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        deletePost: (id) => { dispatch({type: 'DELETE_POST', id:id}) }
+        deletePost: (id) => { dispatch(deletePost(id)) }
     }
 }
 
